662e9189981b286522612cdcd27be1f47d547b71
harui2019 <harui2019@proton.me>
2025-06-25 19:20:37 +0800

    feat(deploy): add direct use deploy folder

--- deploy.py
+++ deploy.py
@@ -13,6 +13,8 @@ class MyProgramArgs(argparse.Namespace):
 
     local: bool = False
     """Run the deployment locally without pushing to GitHub Pages."""
+    use_deploy: bool = False
+    """Use the deploy directory for deployment, not copying from build."""
 
 
 with open("CNAME", "r", encoding="utf-8") as f:
@@ -21,6 +23,28 @@ with open("CNAME", "r", encoding="utf-8") as f:
 CURRENT_DATE = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 print(f"| Deploying documentation to GitHub Pages at {CURRENT_DATE}...")
 print(f"| Custom domain: {CNAME}")
+
+
+def deploy():
+    """Deploy the documentation to GitHub Pages."""
+    if not os.path.exists("./deploy"):
+        print(
+            "| Error: The deploy directory does not exist. "
+            "Please create it or run the build command first."
+        )
+        sys.exit(1)
+
+    ghp_import(
+        "./deploy",
+        nojekyll=True,
+        force=True,
+        push=True,
+        cname=CNAME,
+        mesg=f"ci: deploy to GitHub Pages - {CURRENT_DATE}",
+        branch="gh-pages",
+    )
+    print("| Deployment complete.")
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
@@ -34,8 +58,25 @@ if __name__ == "__main__":
         default=False,
         help="Run the deployment locally without pushing to GitHub Pages.",
     )
+    parser.add_argument(
+        "-d",
+        "--use-deploy",
+        action="store_true",
+        default=False,
+        help="Use the deploy directory for deployment, not copying from build.",
+    )
     nsp = MyProgramArgs()
     args = parser.parse_args(namespace=nsp)
+
+    if args.use_deploy:
+        if args.local:
+            print(
+                "| Warning: The --use-deploy option is set, but the --local option is also set. "
+                "The deployment will not be pushed to GitHub Pages."
+            )
+            sys.exit(1)
+        deploy()
+        sys.exit(0)
 
     print("| Creating temporary deployment directory...")
     if os.path.exists("./deploy"):
@@ -55,9 +96,7 @@ if __name__ == "__main__":
         sys.exit(1)
 
     print("| Copying files from build to deploy directory...")
-
     shutil.copytree("./build", "./deploy")
-
     print("| Moving stable files from deploy/stable to deploy directory...")
 
     STABLE_SOURCE_DIR = "./deploy/stable"
@@ -71,14 +110,6 @@ if __name__ == "__main__":
     print(f"| Moving stable from {STABLE_SOURCE_DIR} to {STABLE_DEPLOY_DIR}...")
 
     if not args.local:
-        ghp_import(
-            "./deploy",
-            nojekyll=True,
-            force=True,
-            push=True,
-            cname=CNAME,
-            mesg=f"ci: deploy to GitHub Pages - {CURRENT_DATE}",
-            branch="gh-pages",
-        )
+        deploy()
 
     print("| Deployment complete.")
